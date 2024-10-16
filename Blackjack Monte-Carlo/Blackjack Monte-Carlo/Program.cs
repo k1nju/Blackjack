c365f72d909c4aaf1af683f96b0cb1ec8e637640
k1nju <150603629+k1nju@users.noreply.github.com>
2024-10-16 21:09:26 +0300

    +analyzer

--- Blackjack Monte-Carlo/Blackjack Monte-Carlo/Program.cs
+++ Blackjack Monte-Carlo/Blackjack Monte-Carlo/Program.cs
@@ -61,6 +61,14 @@ public class Card
     }
 }
 
+public static class RandomHelper
+    {
+        static Random rnd = new Random();
+        public static int Next()
+        {
+            return rnd.Next();
+        }
+    }
 public class Deck
 {
     private List<Card> _cards;
@@ -167,99 +175,99 @@ public class Dealer : Participant
     }
 }
 
-//public class GameAnalyzer
-//{
-//    private int _numberOfSimulations;
-//    private int _playerWins;
-//    private int _dealerWins;
-//    private int _ties;
+public class GameAnalyzer
+{
+    private int _numberOfSimulations;
+    private int _playerWins;
+    private int _dealerWins;
+    private int _ties;
 
-//    public GameAnalyzer(int numberOfSimulations)
-//    {
-//        _numberOfSimulations = numberOfSimulations;
-//    }
+    public GameAnalyzer(int numberOfSimulations)
+    {
+        _numberOfSimulations = numberOfSimulations;
+    }
 
-//    public void Run()
-//    {
-//        for (int i = 0; i < _numberOfSimulations; i++)
-//        {
-//            var result = SimulateGame();
-//            switch (result)
-//            {
-//                case GameResult.PlayerWin:
-//                    _playerWins++;
-//                    break;
-//                case GameResult.DealerWin:
-//                    _dealerWins++;
-//                    break;
-//                case GameResult.Tie:
-//                    _ties++;
-//                    break;
-//            }
-//        }
+    public void Run()
+    {
 
-//        Console.WriteLine($"Games Simulated: {_numberOfSimulations}");
-//        Console.WriteLine($"Player Wins: {_playerWins} ({(_playerWins / (float)_numberOfSimulations) * 100:F2}%)");
-//        Console.WriteLine($"Dealer Wins: {_dealerWins} ({(_dealerWins / (float)_numberOfSimulations) * 100:F2}%)");
-//        Console.WriteLine($"Ties: {_ties} ({(_ties / (float)_numberOfSimulations) * 100:F2}%)");
-//    }
+        for (int i = 0; i < _numberOfSimulations; i++)
+        {
+            var result = SimulateGame();
+            switch (result)
+            {
+                case GameResult.PlayerWin:
+                    _playerWins++;
+                    break;
+                case GameResult.DealerWin:
+                    _dealerWins++;
+                    break;
+                case GameResult.Tie:
+                    _ties++;
+                    break;
+            }
+        }
 
-//    private GameResult SimulateGame()
-//    {
-//        var deck = new Deck();
-//        var player = new Player();
-//        var dealer = new Dealer();
+        Console.WriteLine($"Games Simulated: {_numberOfSimulations}");
+        Console.WriteLine($"Player Wins: {_playerWins} ({(_playerWins / (float)_numberOfSimulations) * 100:F2}%)");
+        Console.WriteLine($"Dealer Wins: {_dealerWins} ({(_dealerWins / (float)_numberOfSimulations) * 100:F2}%)");
+        Console.WriteLine($"Ties: {_ties} ({(_ties / (float)_numberOfSimulations) * 100:F2}%)");
+    }
 
-//        // Начальная раздача
-//        player.TakeCard(deck.DrawCard());
-//        player.TakeCard(deck.DrawCard());
-//        dealer.TakeCard(deck.DrawCard());
-//        dealer.TakeCard(deck.DrawCard());
+    private GameResult SimulateGame()
+    {
+        var deck = new Deck();
+        var player = new Player();
+        var dealer = new Dealer();
+        // Начальная раздача
+        player.TakeCard(deck.DrawCard());
+        player.TakeCard(deck.DrawCard());
+        dealer.TakeCard(deck.DrawCard());
+        dealer.TakeCard(deck.DrawCard());
 
-//        // Ход игрока (базовая стратегия: тянуть карту, если меньше 17, и стоять, если 17 или больше)
-//        while (player.GetHandValue() < 17)
-//        {
-//            player.TakeCard(deck.DrawCard());
-//        }
+        // Ход игрока (базовая стратегия: тянуть карту, если меньше 17, и остановиться, если 17 или больше)
+        while (player.GetHandValue() < 17)
+        {
+            player.TakeCard(deck.DrawCard());
+        }
 
-//        if (player.IsBusted)
-//        {
-//            return GameResult.DealerWin;
-//        }
+        if (player.IsBusted)
+        {
+            return GameResult.DealerWin;
+        }
 
-//        // Ход дилера
-//        dealer.PlayTurn(deck);
+        // Ход дилера
+        dealer.PlayTurn(deck);
 
-//        if (dealer.IsBusted)
-//        {
-//            return GameResult.PlayerWin;
-//        }
+        if (dealer.IsBusted)
+        {
+            return GameResult.PlayerWin;
+        }
 
-//        // Определение победителя
-//        int playerHandValue = player.GetHandValue();
-//        int dealerHandValue = dealer.GetHandValue();
+        // Определение победителя
+        int playerHandValue = player.GetHandValue();
+        int dealerHandValue = dealer.GetHandValue();
 
-//        if (playerHandValue > dealerHandValue)
-//        {
-//            return GameResult.PlayerWin;
-//        }
-//        else if (playerHandValue < dealerHandValue)
-//        {
-//            return GameResult.DealerWin;
-//        }
-//        else
-//        {
-//            return GameResult.Tie;
-//        }
-//    }
+        if (playerHandValue > dealerHandValue)
+        {
+            return GameResult.PlayerWin;
+        }
+        else if (playerHandValue < dealerHandValue)
+        {
+            return GameResult.DealerWin;
+        }
+        else
+        {
+            return GameResult.Tie;
+        }
+    }
 
-//    private enum GameResult
-//    {
-//        PlayerWin,
-//        DealerWin,
-//        Tie
-//    }
-//}
+    private enum GameResult
+    {
+        PlayerWin,
+        DealerWin,
+        Tie
+    }
+}
 public class BlackjackGame
 {
     private Deck _deck;
@@ -348,6 +356,10 @@ public class BlackjackGame
     {
         BlackjackGame game = new BlackjackGame();
         game.StartGame();
+        const int simulations = 10000;
+        GameAnalyzer analyzer = new GameAnalyzer(simulations);
+        analyzer.Run();
         Console.ReadKey();
     }
+
 }
